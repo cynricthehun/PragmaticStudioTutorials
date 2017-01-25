69b949e9093c1cd2b663dc5d6c13dcefcecb43fa
Joey Meyer <joeymeyer1986@gmail.com>
2017-01-25 02:04:03 -0600

    added output for Player Data.

--- studio_game.rb
+++ studio_game.rb
@@ -9,23 +9,27 @@ a way to identify each line as a comment.
 # Author: Joey Meyer
 # Date: January 25th 2017
 
+class Player
+  def initialize(name, health)
+    @player_name=name
+    @player_health=health
+  end
+  def display_details
+    puts "Player Name: #{@player_name}"
+    puts "Player Health: #{@player_health}"
+  end
+end
+
 greeting = "Welcome!"
 timeExecuted = Time.now
 3.times do
   puts "#{greeting.upcase} you started this program at #{timeExecuted}"
 end
 
-class Player
-  def initialize(name, health)
-    @player_name=name
-    @player_health=health
-  end
-end
-
 player1 = Player.new("Larry", 60)
-player2 = Player.new("Curly", 60)
-player3 = Player.new("Moe", 60)
+player2 = Player.new("Curly", 100)
+player3 = Player.new("Moe", 100)
 
-player1.initialize
-player2.initialize
-player3.initialize
+player1.display_details
+player2.display_details
+player3.display_details
