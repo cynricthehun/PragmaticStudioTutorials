f3f32dd3da039fe31e634d4979f1d5334dbaa75b
Joey Meyer <joeymeyer1986@gmail.com>
2017-01-25 18:36:17 -0600

    added multiplication and divition to the short tutorial project.

--- studio_game.rb
+++ studio_game.rb
@@ -8,15 +8,26 @@ a way to identify each line as a comment.
 ##
 # Author: Joey Meyer
 # Date: January 25th 2017
+#
+# to run this application run this command in the directory
+# the ruby file resides in. "Ruby studio_game.rb"
 
 class Player
   def initialize(name, health)
     @player_name=name
     @player_health=health
   end
-  def display_details
-    puts "Player Name: #{@player_name}"
-    puts "Player Health: #{@player_health}"
+  def display_health
+    puts "\n#{@player_name}'s Health: #{@player_health}\n"
+  end
+  def display_name
+    puts "\t#{@player_name.downcase}"
+  end
+  def unformated_name
+    @player_name
+  end
+  def unformated_health
+    @player_health
   end
 end
 
@@ -30,6 +41,16 @@ player1 = Player.new("Larry", 60)
 player2 = Player.new("Curly", 100)
 player3 = Player.new("Moe", 100)
 
-player1.display_details
-player2.display_details
-player3.display_details
+puts player1.display_health
+
+puts "Players:"
+player1.display_name
+player2.display_name
+player3.display_name
+
+puts "\n#{player2.unformated_name} casts Strength on #{player1.unformated_name}"
+puts "#{player1.unformated_name} feels a growing power within himself!"
+puts "#{player1.unformated_name}'s health has Tripled. it is now, #{player1.unformated_health * 3}"
+puts "#{player1.unformated_name} picks a fight with a badger. \nThe badger bites #{player1.unformated_name} and his health is now #{(player1.unformated_health * 3) / 9}. \nThe bite divided his health by nine."
+
+puts "\n Pragmatic Studio Ruby Programming part three finished. \n Numbers and Strings."
